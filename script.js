121a6df9e2f83a135d76b42aba0367b80545da76
Girverius <gsmac1224@gmail.com>
2020-10-14 21:59:33 -0500

    Added confirm and prompt

--- script.js
+++ script.js
@@ -15,7 +15,18 @@ function writePassword() {
 }
 
 function generatePassword() {
+    //prompt for how many characters for password criteria 
+
+    var passwordLength = prompt("Please enter password length you desire. Length must be between 8 and 128 characters.")
+    var upCase = confirm("Do you want upper case letters?");
+    var lowCase = confirm("Do you want lower case letters?");
+    var specChar = confirm("Do you want special characters?");
+    var numb = confirm("Do you want numbers?");
     
+
+    if (condition) {
+        
+    }
 }
 
 // Add event listener to generate button
