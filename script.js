d75960ba5a938294be0b47e238278ca13927474f
Girverius <gsmac1224@gmail.com>
2020-10-15 17:42:55 -0500

    structure changes

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var specialChar = "!@#$%^&*()_-+=;/?.,><";
 var numbers = "1234567890";
 
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -18,13 +20,19 @@ function generatePassword() {
     //prompt for how many characters for password criteria 
     var passwordLength = prompt("Please enter password length you desire. Length must be between 8 and 128 characters.");
     
+    //user confirm what characters he or she wants
+    var upCaseConfirm = confirm("Do you want upper case letters?");
+    var lowCaseConfirm = confirm("Do you want lower case letters?");
+    var specCharConfirm = confirm("Do you want special characters?");
+    var numbConfirm = confirm("Do you want numbers?");
+
     //if statement for using uppercase, lowercase, numbers, and special characters
     if (passwordLength >=8 && passwordLength <=128 && upCaseConfirm && lowCaseConfirm && specCharConfirm && numbConfirm)  {
         function generate(length = passwordLength){
             var all = upperCase + lowerCase + specialChar + numbers;
-            var password ="";
                 for (var i = 0; i < length; i++){
-                    var character = Math.floor(Math.random() * )
+                    var character = Math.floor(Math.random() * all.length);
+                    
                 }
         }
     }
@@ -33,12 +41,7 @@ function generatePassword() {
     }
         
     
-    //user confirm what characters he or she wants
-    var upCaseConfirm = confirm("Do you want upper case letters?");
-    var lowCaseConfirm = confirm("Do you want lower case letters?");
-    var specCharConfirm = confirm("Do you want special characters?");
-    var numbConfirm = confirm("Do you want numbers?");
-    
+   
 
     if (condition) {
         
