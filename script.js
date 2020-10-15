a1194086bb4b5ee666027f321a810f830ac0422c
Girverius <gsmac1224@gmail.com>
2020-10-14 22:52:34 -0500

    Added if and else statements

--- script.js
+++ script.js
@@ -16,12 +16,28 @@ function writePassword() {
 
 function generatePassword() {
     //prompt for how many characters for password criteria 
-
-    var passwordLength = prompt("Please enter password length you desire. Length must be between 8 and 128 characters.")
-    var upCase = confirm("Do you want upper case letters?");
-    var lowCase = confirm("Do you want lower case letters?");
-    var specChar = confirm("Do you want special characters?");
-    var numb = confirm("Do you want numbers?");
+    var passwordLength = prompt("Please enter password length you desire. Length must be between 8 and 128 characters.");
+    
+    //if statement for using uppercase, lowercase, numbers, and special characters
+    if (passwordLength >=8 && passwordLength <=128 && upCaseConfirm && lowCaseConfirm && specCharConfirm && numbConfirm)  {
+        function generate(length = passwordLength){
+            var all = upperCase + lowerCase + specialChar + numbers;
+            var password ="";
+                for (var i = 0; i < length; i++){
+                    var character = Math.floor(Math.random() * )
+                }
+        }
+    }
+    else    {
+        alert("Please enter a password length between 8 and 128!")
+    }
+        
+    
+    //user confirm what characters he or she wants
+    var upCaseConfirm = confirm("Do you want upper case letters?");
+    var lowCaseConfirm = confirm("Do you want lower case letters?");
+    var specCharConfirm = confirm("Do you want special characters?");
+    var numbConfirm = confirm("Do you want numbers?");
     
 
     if (condition) {
